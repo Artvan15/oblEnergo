d3af5c2eff8081df5df646155cbe06efce3ae207
Artvan15 <al_chechot@ukr.net>
2020-06-08 20:31:42 +0300

    Create oblEnergo.sln
    Added solution

---
+++ oblEnergo/oblEnergo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oblEnergo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
