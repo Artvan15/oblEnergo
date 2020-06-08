ad3e085309fb4fd5fb0c22b1df8139e67adeaba1
Artvan15 <al_chechot@ukr.net>
2020-06-08 20:34:36 +0300

    Update Program.cs
    changes to program

--- oblEnergo/oblEnergo/Program.cs
+++ oblEnergo/oblEnergo/Program.cs
@@ -6,7 +6,7 @@ namespace oblEnergo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("It's Laba6!");
         }
     }
 }
