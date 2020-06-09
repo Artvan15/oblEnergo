63e4bfc713353b31dee757b7f607fdc09d51d455
Artvan15 <al_chechot@ukr.net>
2020-06-09 19:45:17 +0300

    Added tests for create, get, delete methods

--- oblEnergo/DAL.Tests/UnitTest1.cs
+++ oblEnergo/DAL.Tests/UnitTest1.cs
@@ -21,9 +21,84 @@ namespace DAL.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Create_InputRegionInstance_CalledAddMethodOfDBSetWithRegionInstance()
         {
+            //create options
+            DbContextOptions options = new DbContextOptionsBuilder<oblEnergoContext>().Options;
+
+            //create double oblEnergoContext, DbSet<Region>
+            var mockContext = new Mock<oblEnergoContext>(options);
+            var mockDbSet = new Mock<DbSet<Region>>();
+
+            //adjust setup so it returns mockDbSet::DbSet<Region> if oblEnergoContext::Set<Region>() called
+            mockContext.Setup(context => context.Set<Region>()).Returns(mockDbSet.Object);
+
+            //create TestRegionRepository and object Region
+            var repository = new TestRegionRepository(mockContext.Object);
+            Region region = new Mock<Region>().Object;
+
+            //call Create
+            repository.Create(region);
+
+            //verify that method Add was called with parameter 'region' once
+            mockDbSet.Verify(dbSet => dbSet.Add(region), Times.Once());
 
         }
+        [Fact]
+        public void Get_InputId_CalledFindMethodOfDBSetWithCorrectId()
+        {
+            // Arrange
+            DbContextOptions options = new DbContextOptionsBuilder<oblEnergoContext>().Options;
+            var mockContext = new Mock<oblEnergoContext>(options);
+            var mockDbSet = new Mock<DbSet<Region>>();
+
+            //adjust setup so it returns mockDbSet::DbSet<Region> if oblEnergoContext::Set<Region>() called
+            mockContext.Setup(context => context.Set<Region>()).Returns(mockDbSet.Object);
+
+            //create object Region
+            Region region = new Region() { Id = 1 };
+
+            //adjust setup so it returns Region object if DbSet<Region>::Find called
+            mockDbSet.Setup(mock => mock.Find(region.Id)).Returns(region);
+            var repository = new TestRegionRepository(mockContext.Object);
+
+            //call Get (that call Find)
+            var actualRegion = repository.Get(region.Id);
+
+            //Verify that method DbSet<Region>::Find(Region::id) with correct argument was called once
+            mockDbSet.Verify(dbSet => dbSet.Find(region.Id), Times.Once());
+
+            //Verify that DbSet<Region>::Find returns the same object as DbSet<Region>::Get
+            Assert.Equal(region, actualRegion);
+        }
+
+        [Fact]
+        public void Delete_InputId_CalledFindAndRemoveMethodsOfDBSetWithCorrectArg()
+        {
+            // Arrange
+            DbContextOptions options = new DbContextOptionsBuilder<oblEnergoContext>().Options;
+            var mockContext = new Mock<oblEnergoContext>(options);
+            var mockDbSet = new Mock<DbSet<Region>>();
+
+            //adjust setup so it returns mockDbSet::DbSet<Region> if oblEnergoContext::Set<Region>() called
+            mockContext.Setup(context => context.Set<Region>()).Returns(mockDbSet.Object);
+
+            var repository = new TestRegionRepository(mockContext.Object);
+
+            //create Region object
+            Region region = new Region() { Id = 1 };
+            //adjust setup so it returns Region object if DbSet<Region>::Find called
+            mockDbSet.Setup(mock => mock.Find(region.Id)).Returns(region);
+
+            //call Delete (that call Get)
+            repository.Delete(region.Id);
+
+            //Verify that method DbSet<Region>::Find(Region::id) with correct argument was called once
+            mockDbSet.Verify(dbSet => dbSet.Find(region.Id), Times.Once());
+
+            //Verify that method DbSet<Region>::Remove(Region) with correct argument was called once
+            mockDbSet.Verify(dbSet => dbSet.Remove(region), Times.Once());
+        }
+
     }
 }
--- oblEnergo/DAL/Repositories/Impl/BaseRepository.cs
+++ oblEnergo/DAL/Repositories/Impl/BaseRepository.cs
@@ -14,46 +14,35 @@ namespace DAL.Repositories.Impl
     {
         private readonly DbSet<T> _set;
         private readonly DbContext _context;
-​
         public BaseRepository(DbContext context)
         {
             _context = context;
             _set = context.Set<T>();
         }
-​
         public void Create(T item)
         {
             _set.Add(item);
         }
-​
+
         public void Delete(int id)
         {
             var item = Get(id);
             _set.Remove(item);
         }
-​
-        public IEnumerable<T> Find(
-            Func<T, bool> predicate,
-            int pageNumber = 0,
-            int pageSize = 10)
+        public IEnumerable<T> Find
+            (Func<T, bool> predicate, int pageNumber = 0, int pageSize = 10)
         {
             return
-                _set.Where(predicate)
-                    .Skip(pageSize * pageNumber)
-                    .Take(pageNumber)
-                    .ToList();
+                _set.Where(predicate).Skip(pageSize * pageNumber).Take(pageNumber).ToList();
         }
-​
         public T Get(int id)
         {
             return _set.Find(id);
         }
-​
         public IEnumerable<T> GetAll()
         {
             return _set.ToList();
         }
-​
         public void Update(T item)
         {
             _context.Entry(item).State = EntityState.Modified;
