a63f29df62fa4665f440e609fc2f8aa893b9e17b
Artvan15 <al_chechot@ukr.net>
2020-06-09 16:40:30 +0300

    Added moq

--- oblEnergo/DAL.Tests/UnitTest1.cs
+++ oblEnergo/DAL.Tests/UnitTest1.cs
@@ -1,8 +1,23 @@
 using System;
 using Xunit;
+using DAL.Repositories.Impl;
+using DAL.EF;
+using Microsoft.EntityFrameworkCore;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+using Moq;
 
 namespace DAL.Tests
 {
+    class TestRegionRepository
+        : BaseRepository<Region>
+    {
+        public TestRegionRepository(DbContext context)
+            : base(context)
+        {
+
+        }
+    }
     public class UnitTest1
     {
         [Fact]
